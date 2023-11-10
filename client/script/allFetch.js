4d7525d0ac37bb3ccbb632cb469eae0aadb259b0
felipeflamarini <felipeflamarini@hotmail.com>
2023-11-10 12:01:58 -0300

    funções para integrar com db

--- client/script/allFetch.js
+++ client/script/allFetch.js
@@ -1,7 +1,27 @@
 async function getAllProducts() {
-    return await ((await fetch('http://localhost:3000/api/products/all')).json());
+    return await fetch ('http://localhost:3000/api/products/all')
+    .then(async response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json();
+    })
 };
 
+async function getProductById(id) {
+    await fetch(`http://localhost:3000/api/products/${id}`)
+    .then(async response => {
+        console.log(response)
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        else return await response;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
 async function getRecommended() {
-    
+    console.log(await getAllProducts());
 }
--- client/script/index.js
+++
@@ -1,5 +0,0 @@
-async function getRecommended() {
-    getAllProducts().then((data) => {
-        console.log(data);
-    });
-}

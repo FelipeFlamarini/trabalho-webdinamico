4dc7808d4339610743cddde0232dbf78c6f76256
felipeflamarini <felipeflamarini@hotmail.com>
2023-11-14 01:51:43 -0300

    estilizando index, carrosseis funcionando corretamente, falta colorir

--- client/script/index.js
+++ client/script/index.js
@@ -5,104 +5,60 @@ async function updateRecommended() {
 };
 
 async function updateMostViewed(limit) {
-    const mostViewed = await getProductsMostViewed(limit);
-    console.log(mostViewed);
-
-    mostViewed.forEach((product) => {
-        const li = document.createElement('li');
-        li.classList.add('glide__slide');
-        li.classList.add('card');
-
-    
-        li.innerHTML = `
-        <div>
-            <a href="/product/${product.id}">
-                <img src="http://localhost:3000/api/productImages/${product.id}/1.jpg" alt="${product.nome}">
-                <h3>${product.nome}</h3>
-                <p>R$ ${product.preco}</p>
-            </a>
-        </div>
-        `;
-    
-        document.querySelector('.maisVistos .glide__slides').appendChild(li);
+    await getProductsMostViewed(limit)
+    .then((products) => {
+        createCards(products, '.maisVistos');
     });
 };
 
 async function updateMostSold(limit) {
-    const mostSold = await getProductsMostSold(limit);
-    console.log(mostSold);
-
-    mostSold.forEach((product) => {
-        const li = document.createElement('li');
-        li.classList.add('glide__slide');
-        li.classList.add('card');
-    
-        li.innerHTML = `
-        <div>
-            <a href="/product/${product.id}">
-                <img src="http://localhost:3000/api/productImages/${product.id}/1.jpg" alt="${product.nome}">
-                <h3>${product.nome}</h3>
-                <p>R$ ${product.preco}</p>
-            </a>
-        </div>
-        `;
-    
-        document.querySelector('.maisVendidos .glide__slides').appendChild(li);
+    await getProductsMostSold(limit)
+    .then((products) => {
+        createCards(products, '.maisVendidos');
     });
 };
 
 async function updateLeastStock(limit) {
-    const leastStock = await getProductsLeastStock(limit);
-    console.log(leastStock);
-
-    leastStock.forEach((product) => {
-        const li = document.createElement('li');
-        li.classList.add('glide__slide');
-        li.classList.add('card');
-    
-        li.innerHTML = `
-            <a href="/product/${product.id}">
-                <img src="http://localhost:3000/api/productImages/${product.id}/1.jpg" alt="${product.nome}">
-                <h3>${product.nome}</h3>
-                <p>R$ ${product.preco}</p>
-            </a>
-        `;
-    
-        document.querySelector('.menosEstoque .glide__slides').appendChild(li);
+    await getProductsLeastStock(limit)
+    .then((products) => {
+        createCards(products, '.menosEstoque');
     });
 };
 
+async function createCards(products, className) {
+    await products.forEach((product, index) => {
+        const li = document.createElement('li');
+        li.classList.add('glide__slide');
+        li.classList.add('card');
+
+        li.innerHTML = `
+            <a href="/product/${product.id}">
+                <img src="http://localhost:3000/api/productImages/${product.id}/1.jpg" alt="${product.nome}">
+                <div>
+                    <h3>${product.nome}</h3>
+                    <p>R$ ${product.preco}</p>
+                </div>
+            </a>
+        `;
+    
+        document.querySelector(`${className} .glide__slides`).appendChild(li);
+        document.querySelector(`${className} .glide__bullets`).innerHTML += 
+            `<button class="glide__bullet" data-glide-dir="=${index}">
+                <i class="fa-solid fa-circle"></i>
+            </button>
+        `;
+    });
+    new Glide(document.querySelector(className), {
+        type: 'carousel',
+        perView: 4,
+        gap: 20,
+        autoplay: 5000,
+        // length: 10,
+        focusAt: 'center',
+    }).mount();
+}
+
 // updateRecommended();
-updateMostSold(8)
-.then(() => {
-    // glidejs
-    document.querySelectorAll('.maisVendidos').forEach((el) => {
-        new Glide(el, {
-            type: 'carousel',
-            perView: 5,
-            gap: 0,
-        }).mount();
-    });
-});
-
-updateMostViewed(8)
-.then(() => {
-    // glidejs
-    document.querySelectorAll('.maisVistos').forEach((el) => {
-        new Glide(el, {
-            type: 'carousel',
-            perView: 5,
-        }).mount();
-    });
-});
-
-updateLeastStock(8)
-.then(() => {
-    // glidejs
-    document.querySelectorAll('.menosEstoque').forEach((el) => {
-        new Glide(el, {
-            type: 'carousel',
-            perView: 5,
-        }).mount();
-    });
-});
+updateMostSold(10);
+updateMostViewed(10);
+updateLeastStock(10);

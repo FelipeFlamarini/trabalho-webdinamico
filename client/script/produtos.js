acf9d8f71dc4758c2cc02265de2ba8626d1188b8
kauan345developer <kauanolival@gmail.com>
2023-11-14 12:57:39 -0300

    terminando estilização grid

--- client/script/produtos.js
+++ client/script/produtos.js
@@ -21,11 +21,14 @@ async function products() {
     // Create the image element
     const img = document.createElement("img");
     img.src = `http://localhost:3000/api/productImages/${produto.id}/1.jpg`;
-    img.alt = produto.name;
+    img.alt = produto.nome;
   
     // Append the image element to the image container element
     imgContainer.appendChild(img);
   
+    const allDetails = document.createElement("div")
+    allDetails.classList.add("all-details")
+
     // Create the details container element
     const detailsContainer = document.createElement("div");
     detailsContainer.classList.add("details");
@@ -40,22 +43,26 @@ async function products() {
   
     // Create the price element
     const price = document.createElement("span");
-    price.textContent = produto.preco;
+    price.textContent =`$${produto.preco}` ;
   
     // Create the button element
     const button = document.createElement("button");
     button.classList.add("cart-button");
     button.textContent = "adicionar ao carrinho";
   
+    
+
     // Append the brand, name, price, and button elements to the details container element
     detailsContainer.appendChild(brand);
     detailsContainer.appendChild(name);
     detailsContainer.appendChild(price);
-    detailsContainer.appendChild(button);
-  
+    
+    allDetails.appendChild(detailsContainer)
+    allDetails.appendChild(button)
+
     // Append the image container and details container elements to the card container element
     cardContainer.appendChild(imgContainer);
-    cardContainer.appendChild(detailsContainer);
+    cardContainer.appendChild(allDetails);
   
     // Append the card container element to the parent element
     parentElement.appendChild(cardContainer);

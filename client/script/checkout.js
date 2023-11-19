735e590bf71caf9d4eb5f41444fbe706a331cc7c
FelipeFlamarini <felipeflamarini@hotmail.com>
2023-11-19 19:46:24 -0300

    checkout finalizado

--- client/script/checkout.js
+++ client/script/checkout.js
@@ -14,13 +14,45 @@ function teste() {
     );
 }
 
+async function paymentMethod() {
+    const metodos = document.querySelectorAll(".metodosPagamento span");
+    metodos[0].classList.add("active");
+    metodos.forEach((metodo, index) => {
+        metodo.onclick = function () {
+            activate(index);
+        };
+    });
+}
+
+function activate(index) {
+    const metodos = document.querySelectorAll(".metodosPagamento span");
+    metodos.forEach((metodo) => {
+        metodo.classList.remove("active");
+    });
+    metodos[index].classList.add("active");
+}
+
+async function getMaxQuantity() {
+    const ids = await getIdsLocalStorage();
+    const max = await Promise.all(
+        ids.map(async (id) => {
+            const res = await fetch(
+                `http://localhost:3000/api/products/${id.id}`
+            );
+            const product = await res.json();
+            return product.estoque;
+        })
+    );
+    return max;
+}
+
 function getIdsLocalStorage() {
     return JSON.parse(localStorage.getItem("productID"));
 }
 
 async function setCards() {
     const ids = await getIdsLocalStorage();
-    ids.forEach(async (id, index) => {
+    return await ids.map(async (id, index) => {
         await fetch(`http://localhost:3000/api/products/${id.id}`)
             .then((res) => res.json())
             .then((product) => {
@@ -32,7 +64,9 @@ async function setCards() {
                     <h2>${product.nome}</h2>
                 </div>
                 `;
-                document.querySelector('.mainContainer').appendChild(nomeProduto);
+                document
+                    .querySelector(".mainContainer")
+                    .appendChild(nomeProduto);
 
                 const quantidadeProduto = document.createElement("div");
                 quantidadeProduto.classList.add("quantidadeProduto");
@@ -41,25 +75,36 @@ async function setCards() {
                 <h2 id=quantity_${index}>${id.quantity}</h2>
                 <button onclick=increment(${index},'quantity')>+</button>
                 `;
-                document.querySelector('.mainContainer').appendChild(quantidadeProduto);
+                document
+                    .querySelector(".mainContainer")
+                    .appendChild(quantidadeProduto);
 
                 const precoFinalProduto = document.createElement("div");
                 precoFinalProduto.classList.add("precoFinalProduto");
                 precoFinalProduto.innerHTML = `
-                <h2 class=price>R$ ${(product.preco * id.quantity).toFixed(2)}</h2>
+                <h2 class=price>R$ ${(product.preco * id.quantity).toFixed(
+                    2
+                )}</h2>
                 `;
-                document.querySelector('.mainContainer').appendChild(precoFinalProduto);
+                document
+                    .querySelector(".mainContainer")
+                    .appendChild(precoFinalProduto);
             });
+        updatePrices();
     });
 }
 
 async function increment(localStorageIndex, key) {
+    const maxQuantity = await getMaxQuantity();
     const ids = await getIdsLocalStorage();
     const quantity = document.querySelector(`#quantity_${localStorageIndex}`);
 
     ids[localStorageIndex][key] += 1;
+    if (ids[localStorageIndex][key] > maxQuantity[localStorageIndex]) {
+        return;
+    }
 
-    localStorage.setItem('productID', JSON.stringify(ids));
+    localStorage.setItem("productID", JSON.stringify(ids));
     quantity.innerHTML = ids[localStorageIndex][key];
 
     updatePrices();
@@ -71,10 +116,10 @@ async function decrement(localStorageIndex, key) {
 
     ids[localStorageIndex][key] -= 1;
     if (ids[localStorageIndex][key] < 1) {
-        return
+        return;
     }
 
-    localStorage.setItem('productID', JSON.stringify(ids));
+    localStorage.setItem("productID", JSON.stringify(ids));
     quantity.innerHTML = ids[localStorageIndex][key];
 
     updatePrices();
@@ -82,14 +127,24 @@ async function decrement(localStorageIndex, key) {
 
 async function updatePrices() {
     const ids = await getIdsLocalStorage();
+    let finalPrice = 0;
 
-    const prices = document.querySelectorAll('.price');
-    ids.forEach(async (id, index) => {
+    const prices = document.querySelectorAll(".price");
+    const promise = await ids.map(async (id, index) => {
         await fetch(`http://localhost:3000/api/products/${id.id}`)
             .then((res) => res.json())
             .then((product) => {
-                prices[index].innerHTML = `R$ ${(product.preco * id.quantity).toFixed(2)}`;
+                finalPrice += product.preco * id.quantity;
+                prices[index].innerHTML = `R$ ${(
+                    product.preco * id.quantity
+                ).toFixed(2)}`;
             });
+    });
+
+    await Promise.all(promise).then(() => {
+        document.querySelector("#total").innerHTML = `R$ ${finalPrice.toFixed(
+            2
+        )}`;
     });
 }
 
@@ -110,14 +165,11 @@ async function checkout() {
                 window.location.assign(
                     "http://localhost:5500/client/sucesso.html"
                 );
-            } else {
-                window.location.assign(
-                    "http://localhost:5500/client/erro.html"
-                );
             }
         }
     );
 }
 
-teste()
+paymentMethod();
+teste();
 setCards();

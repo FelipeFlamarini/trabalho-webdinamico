36096261f382bb8f4f07ad67b26a72e4953ecbf3
FelipeFlamarini <felipeflamarini@hotmail.com>
2023-11-19 11:59:33 -0300

    funçoes prontas, falta criar a página

--- client/script/checkout.js
+++ client/script/checkout.js
@@ -1,9 +1,46 @@
+function teste() {
+    localStorage.setItem(
+        "productID",
+        JSON.stringify([
+            {
+                id: 1,
+                quantity: 1,
+            },
+            {
+                id: 2,
+                quantity: 1,
+            },
+        ])
+    );
+}
+
+function getIdsLocalStorage() {
+    teste();
+    return JSON.parse(localStorage.getItem("productID"));
+}
+
 async function checkout() {
-    const cart = JSON.parse(localStorage.getItem('productID'));
-    const ids = cart.map((product) => {
-        return product.id;
-    });
-    console.log(ids);
-    await cartCheckout(JSON.stringify(ids));
-    localStorage.clear();
+    const ids = await getIdsLocalStorage();
+
+    const reqOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(ids),
+    };
+    await fetch("http://localhost:3000/api/cart/checkout", reqOptions).then(
+        (res) => {
+            console.log(res);
+            if (res.status === 200) {
+                window.location.assign(
+                    "http://localhost:5500/client/sucesso.html"
+                );
+            } else {
+                window.location.assign(
+                    "http://localhost:5500/client/erro.html"
+                );
+            }
+        }
+    );
 }
--- server/router.js
+++ server/router.js
@@ -1,64 +1,83 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import url from 'url';
-import bodyparser from 'body-parser';
-import { getAllProducts, getProductById } from './DB/functions.js';
-import { error } from 'console';
+import express, { json } from "express";
+import cors from "cors";
+import url from "url";
+import bodyparser from "body-parser";
+import { getAllProducts, getProductById } from "./DB/functions.js";
+import { error } from "console";
+import { produtos } from "./DB/structure.js";
 
 const app = express();
 
 app.use(cors());
 app.use(json());
-app.use(bodyparser.urlencoded({ limit: "50mb", extended: false}));
+app.use(bodyparser.urlencoded({ limit: "50mb", extended: false }));
 
 app.get("/api/products/:id", async (req, res) => {
-    const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
 
-    try {
-        if (isNaN(id)) {
-            await getAllProducts()
-            .then((query) => {
-                return query.map((product) => {
-                    return product.dataValues;
-                })
-            })
-            .then((query) => {
-                res.status(200).send(query);
-
-            });
-        } else {
-            await getProductById(id)
-            .then((query) => {
-                res.status(200).send(query);
-            });
-        }
+  try {
+    if (isNaN(id)) {
+      await getAllProducts()
+        .then((query) => {
+          return query.map((product) => {
+            return product.dataValues;
+          });
+        })
+        .then((query) => {
+          res.status(200).send(query);
+        });
+    } else {
+      await getProductById(id).then((query) => {
+        res.status(200).send(query);
+      });
     }
-    catch {
-        console.log(error);
-    }
+  } catch {
+    console.log(error);
+  }
 });
 
 app.put("/api/cart/checkout", async (req, res) => {
-    const { body } = req;
-    body.forEach(async (item) => {
-        await getProductById(item.id)
-        .then((product) => {
-            product.estoque -= item.quantity;
-            product.save();
-        });
-    });
-    res.status(200).send();
-})
+  const { body } = req;
+  const productsOnDB = [];
+  const newEstoque = [];
+
+  const estoque = body.map(async (item) => {
+    const productOnDB = (await getProductById(item.id)).dataValues;
+    productsOnDB.push(productOnDB);
+
+    const tempEstoque = productOnDB.estoque - item.quantity;
+    newEstoque.push(tempEstoque);
+
+    if (tempEstoque < 0) {
+      return false;
+    }
+    return true;
+  });
+
+  if ((await Promise.all(estoque)).includes(false)) {
+    res.status(400).send("Estoque insuficiente");
+  } else {
+    productsOnDB.forEach(async (product, index) => {
+        await produtos.update(
+          { estoque: newEstoque[index] },
+          { where: { id: product.id } }
+        );
+      });
+    res.status(200).send("Compra realizada com sucesso");
+  }
+});
 
 // servindo imagens dos produtos
 app.use("/api/productImages", express.static("./server/public/imgs/produtos"));
-app.use("/api/productImagesTransparent", express.static("./server/public/imgs/produtosTransparent"));
+app.use(
+  "/api/productImagesTransparent",
+  express.static("./server/public/imgs/produtosTransparent")
+);
 
 try {
-    app.listen(3000)
-    console.log("Server running on port 3000");
-}
-catch {
-    console.log("Error on server");
+  app.listen(3000);
+  console.log("Server running on port 3000");
+} catch {
+  console.log("Error on server");
 }
 export default app;

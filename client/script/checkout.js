62f92d6757f623e5be5a0bfb2ab363ffdef41a7b
FelipeFlamarini <felipeflamarini@hotmail.com>
2023-11-19 13:33:48 -0300

    desenvolvendo checkout.html

--- client/script/checkout.js
+++ client/script/checkout.js
@@ -8,15 +8,89 @@ function teste() {
             },
             {
                 id: 2,
-                quantity: 1,
+                quantity: 2,
             },
         ])
     );
 }
 
 function getIdsLocalStorage() {
-    teste();
     return JSON.parse(localStorage.getItem("productID"));
+}
+
+async function setCards() {
+    const ids = await getIdsLocalStorage();
+    ids.forEach(async (id, index) => {
+        await fetch(`http://localhost:3000/api/products/${id.id}`)
+            .then((res) => res.json())
+            .then((product) => {
+                const nomeProduto = document.createElement("div");
+                nomeProduto.classList.add("nomeProduto");
+                nomeProduto.innerHTML = `
+                <img src=http://localhost:3000/api/productImagesTransparent/${product.id}/1.jpg></img>
+                <div>
+                    <h2>${product.nome}</h2>
+                </div>
+                `;
+                document.querySelector('.mainContainer').appendChild(nomeProduto);
+
+                const quantidadeProduto = document.createElement("div");
+                quantidadeProduto.classList.add("quantidadeProduto");
+                quantidadeProduto.innerHTML = `
+                <button onclick=decrement(${index},'quantity')>-</button>
+                <h2 id=quantity_${index}>${id.quantity}</h2>
+                <button onclick=increment(${index},'quantity')>+</button>
+                `;
+                document.querySelector('.mainContainer').appendChild(quantidadeProduto);
+
+                const precoFinalProduto = document.createElement("div");
+                precoFinalProduto.classList.add("precoFinalProduto");
+                precoFinalProduto.innerHTML = `
+                <h2 class=price>R$ ${(product.preco * id.quantity).toFixed(2)}</h2>
+                `;
+                document.querySelector('.mainContainer').appendChild(precoFinalProduto);
+            });
+    });
+}
+
+async function increment(localStorageIndex, key) {
+    const ids = await getIdsLocalStorage();
+    const quantity = document.querySelector(`#quantity_${localStorageIndex}`);
+
+    ids[localStorageIndex][key] += 1;
+
+    localStorage.setItem('productID', JSON.stringify(ids));
+    quantity.innerHTML = ids[localStorageIndex][key];
+
+    updatePrices();
+}
+
+async function decrement(localStorageIndex, key) {
+    const ids = await getIdsLocalStorage();
+    const quantity = document.querySelector(`#quantity_${localStorageIndex}`);
+
+    ids[localStorageIndex][key] -= 1;
+    if (ids[localStorageIndex][key] < 1) {
+        return
+    }
+
+    localStorage.setItem('productID', JSON.stringify(ids));
+    quantity.innerHTML = ids[localStorageIndex][key];
+
+    updatePrices();
+}
+
+async function updatePrices() {
+    const ids = await getIdsLocalStorage();
+
+    const prices = document.querySelectorAll('.price');
+    ids.forEach(async (id, index) => {
+        await fetch(`http://localhost:3000/api/products/${id.id}`)
+            .then((res) => res.json())
+            .then((product) => {
+                prices[index].innerHTML = `R$ ${(product.preco * id.quantity).toFixed(2)}`;
+            });
+    });
 }
 
 async function checkout() {
@@ -44,3 +118,6 @@ async function checkout() {
         }
     );
 }
+
+teste()
+setCards();

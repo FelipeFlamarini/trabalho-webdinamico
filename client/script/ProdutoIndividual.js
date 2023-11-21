a9fbcc3ba272dd40b732a0b95f59d1265f204998
felipeflamarini <felipeflamarini@hotmail.com>
2023-11-21 16:11:30 -0300

    botao de comprar alterado para reagir dependendo da quantidade de estoque

--- client/script/ProdutoIndividual.js
+++ client/script/ProdutoIndividual.js
@@ -1,151 +1,156 @@
 import { addInCart } from "./addInCart.js";
 import { getProductById } from "./allFetch.js";
 
-
 // localStorage.setItem('produtoAtual',JSON.stringify(1))
 
 function produtsReturn() {
-  const arrayOfIds = localStorage.getItem('productID') ? JSON.parse(localStorage.getItem('productID')) : []
-  return arrayOfIds
+    const arrayOfIds = localStorage.getItem("productID")
+        ? JSON.parse(localStorage.getItem("productID"))
+        : [];
+    return arrayOfIds;
 }
 
-function renderProduto(produto){
-  const containerProduto = document.getElementById("container-produtoID");
-// containerProduto.classList.add('container-produto');
+function renderProduto(produto) {
+    const containerProduto = document.getElementById("container-produtoID");
+    // containerProduto.classList.add('container-produto');
 
-const containerImage = document.createElement('div');
-containerImage.classList.add('container-image');
+    const containerImage = document.createElement("div");
+    containerImage.classList.add("container-image");
 
-const image = document.createElement('img');
-image.src = `../server/public/imgs/produtosTransparent/${produto.id}/1.jpg`;
-image.alt = '';
+    const image = document.createElement("img");
+    image.src = `../server/public/imgs/produtosTransparent/${produto.id}/1.jpg`;
+    image.alt = "";
 
-containerImage.appendChild(image);
+    containerImage.appendChild(image);
 
-const productDetails = document.createElement('div');
-productDetails.classList.add('product-details');
+    const productDetails = document.createElement("div");
+    productDetails.classList.add("product-details");
 
-const detailsDiv1 = document.createElement('div');
-const brandSpan = document.createElement('span');
-brandSpan.textContent = produto.universo;
+    const detailsDiv1 = document.createElement("div");
+    const brandSpan = document.createElement("span");
+    brandSpan.textContent = produto.universo;
 
-const title1H1 = document.createElement('h1');
-title1H1.textContent = `${produto.nome.split('!')[0]}!`;
-const title2H1 = document.createElement('h1');
-title2H1.textContent = `${produto.nome.split('!')[1]}`;
-const priceSpan= document.createElement('span');
-priceSpan.id = ('produtoPreco');
-priceSpan.textContent = `R$ ${produto.preco}`;
+    const title1H1 = document.createElement("h1");
+    title1H1.textContent = `${produto.nome.split("!")[0]}!`;
+    const title2H1 = document.createElement("h1");
+    title2H1.textContent = `${produto.nome.split("!")[1]}`;
+    const priceSpan = document.createElement("span");
+    priceSpan.id = "produtoPreco";
+    priceSpan.textContent = `R$ ${produto.preco}`;
 
-detailsDiv1.appendChild(brandSpan);
-detailsDiv1.appendChild(title1H1);
-detailsDiv1.appendChild(title2H1);
-detailsDiv1.appendChild(priceSpan);
+    detailsDiv1.appendChild(brandSpan);
+    detailsDiv1.appendChild(title1H1);
+    detailsDiv1.appendChild(title2H1);
+    detailsDiv1.appendChild(priceSpan);
 
-const detailsDiv2 = document.createElement('div');
-const descriptionP = document.createElement('p');
-descriptionP.textContent = produto.descricao;
+    const detailsDiv2 = document.createElement("div");
+    const descriptionP = document.createElement("p");
+    descriptionP.textContent = produto.descricao;
 
-detailsDiv2.appendChild(descriptionP);
+    detailsDiv2.appendChild(descriptionP);
 
-const detailsDiv3 = document.createElement('div');
-const changesbuttonDiv3 = document.createElement('div')
-changesbuttonDiv3.classList.add("changes-button")
-const cartImage = document.createElement('img');
-cartImage.src = './imgs/cart.svg';
-cartImage.alt = '';
-const buttonElement = document.createElement('button');
-buttonElement.innerHTML = "adicionar ao <br> carrinho"
-buttonElement.addEventListener("click",() =>{
-  console.log("a")
-  addInCart(produto)
-  changesbuttonDiv3.style.display ='flex'
-  buttonElement.style.display= 'none'
-})
-const selectElement = document.createElement('select');
+    const detailsDiv3 = document.createElement("div");
+    const changesbuttonDiv3 = document.createElement("div");
+    changesbuttonDiv3.classList.add("changes-button");
+    const cartImage = document.createElement("img");
+    cartImage.src = "./imgs/cart.svg";
+    cartImage.alt = "";
+    const buttonElement = document.createElement("button");
+    if (produto.estoque) {
+        buttonElement.innerHTML = "adicionar ao <br> carrinho";
+        buttonElement.addEventListener("click", () => {
+            console.log("a");
+            addInCart(produto);
+            changesbuttonDiv3.style.display = "flex";
+            buttonElement.style.display = "none";
+        });
+    } else {
+        buttonElement.innerHTML = "produto <br> indisponível";
+        buttonElement.disabled = true;
+    }
 
-const arrayOfIds = localStorage.getItem('productID') ? JSON.parse(localStorage.getItem('productID')) : []
-const purchasedProduct = arrayOfIds.find(item => item.id == produto.id);
+    const selectElement = document.createElement("select");
 
-if(purchasedProduct){
-  changesbuttonDiv3.style.display ='flex'
-  buttonElement.style.display= 'none'
+    const arrayOfIds = localStorage.getItem("productID")
+        ? JSON.parse(localStorage.getItem("productID"))
+        : [];
+    const purchasedProduct = arrayOfIds.find((item) => item.id == produto.id);
+
+    if (purchasedProduct) {
+        changesbuttonDiv3.style.display = "flex";
+        buttonElement.style.display = "none";
+    }
+
+    const valueSelect = purchasedProduct
+        ? purchasedProduct.quantity
+        : undefined;
+
+    for (let index = 1; index <= produto.estoque && index < 16; index++) {
+        const option = document.createElement("option");
+        option.setAttribute("value", index);
+        option.textContent = index;
+
+        if (index === valueSelect) {
+            console.log(index);
+            option.setAttribute("selected", "selected");
+        }
+
+        selectElement.appendChild(option);
+    }
+
+    selectElement.addEventListener("change", (ev) => {
+        const arrayOfIds = localStorage.getItem("productID")
+            ? JSON.parse(localStorage.getItem("productID"))
+            : [];
+
+        const found = arrayOfIds.find(
+            (produtoLocal) => produtoLocal.id === produto.id
+        );
+
+        if (found) {
+            found.quantity = +ev.target.value;
+            localStorage.setItem("productID", JSON.stringify(arrayOfIds));
+        }
+    });
+
+    const spanElement = document.createElement("span");
+    spanElement.textContent = "Adicionado";
+    const trashImage = document.createElement("img");
+    trashImage.src = "./imgs/individualTrash.svg";
+    trashImage.alt = "";
+    trashImage.addEventListener("click", () => {
+        const arrayOfIds = produtsReturn();
+        const filterArray = arrayOfIds.filter((p) => p.id !== produto.id);
+        localStorage.setItem("productID", JSON.stringify(filterArray));
+        changesbuttonDiv3.style.display = "none";
+        buttonElement.style.display = "block";
+    });
+
+    detailsDiv3.appendChild(cartImage);
+    detailsDiv3.appendChild(buttonElement);
+    detailsDiv3.appendChild(changesbuttonDiv3);
+
+    changesbuttonDiv3.appendChild(selectElement);
+    changesbuttonDiv3.appendChild(spanElement);
+    changesbuttonDiv3.appendChild(trashImage);
+
+    productDetails.appendChild(detailsDiv1);
+    productDetails.appendChild(detailsDiv2);
+    productDetails.appendChild(detailsDiv3);
+
+    containerProduto.appendChild(containerImage);
+    containerProduto.appendChild(productDetails);
 }
-
-const valueSelect = purchasedProduct ? purchasedProduct.quantity : undefined;
-
-for (let index = 1; index < 16; index++) {
-  const option = document.createElement('option');
-      option.setAttribute('value', index);
-      option.textContent = index
-      
-      if (index === valueSelect) {
-        console.log(index)
-        option.setAttribute('selected', 'selected');
-      }
-
-      selectElement.appendChild(option);
-  
-}
-
-selectElement.addEventListener("change",(ev)=>{
-  const arrayOfIds = localStorage.getItem('productID') ? JSON.parse(localStorage.getItem('productID')) : []
-  
-  const found = arrayOfIds.find(produtoLocal => produtoLocal.id === produto.id);
-  
-  if(found){
-    found.quantity = +ev.target.value;
-    localStorage.setItem('productID', JSON.stringify(arrayOfIds))
-  }
-
-})
-
-
-const spanElement = document.createElement('span')
-spanElement.textContent ="Adicionado"
-const trashImage = document.createElement('img');
-trashImage.src = './imgs/individualTrash.svg';
-trashImage.alt = '';
-trashImage.addEventListener('click',()=>{
-  const arrayOfIds = produtsReturn()
-  const filterArray = arrayOfIds.filter( p => p.id !== produto.id)
-  localStorage.setItem('productID', JSON.stringify(filterArray))
-  changesbuttonDiv3.style.display ='none'
-  buttonElement.style.display= 'block'
-})
-
-
-
-detailsDiv3.appendChild(cartImage);
-detailsDiv3.appendChild(buttonElement);
-detailsDiv3.appendChild(changesbuttonDiv3)
-
-changesbuttonDiv3.appendChild(selectElement);
-changesbuttonDiv3.appendChild(spanElement)
-changesbuttonDiv3.appendChild(trashImage);
-
-productDetails.appendChild(detailsDiv1);
-productDetails.appendChild(detailsDiv2);
-productDetails.appendChild(detailsDiv3);
-
-containerProduto.appendChild(containerImage);
-containerProduto.appendChild(productDetails);
-}
-
-
 
 // Append containerProduto to the desired parent element
 
+async function render() {
+    const id = window.location.search.split("id=")[1];
+    // se não tiver id
+    if (!id) window.location.href = "./home.html";
 
-
-async function render(){
-  const id = window.location.search.split('id=')[1];
-  // se não tiver id
-  if (!id) window.location.href = './home.html'; 
-
-  const atualProduto = await getProductById(id)
-  console.log(atualProduto)
-  renderProduto(atualProduto)
+    const atualProduto = await getProductById(id);
+    renderProduto(atualProduto);
 }
 
-render()
+render();
